f862fd331deed11a476de4c41a0cfeadc89180f6
keshav <engrkeshavkushwah@gmail.com>
2025-07-15 23:32:05 +0530

    home page completed

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import assets from '../assets/assets'
+import React, { useEffect, useRef } from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+ 
+   const scrollEnd = useRef()
+
+   useEffect(()=>{
+    if(scrollEnd.current){
+      scrollEnd.current.scrollIntoView({behavior : "smooth"})
+    }
+   },[])
+ 
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      {/* ......{header}....... */}
          <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
           <img src={assets.profile_martin} alt="" className='w-8 rounded-full'/>
           <p className='flex-1 text-lg text-white flex items-center gap-2'>
@@ -14,6 +25,44 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
           className='md:hidden max-w-7'/>
           <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5'/>
          </div>
+         {/* .....chat Area..... */}
+         <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+           {messagesDummyData.map((msg, index) => (
+            <div key={index} className={`flex items-end gap-2 justify-end 
+            ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+              {msg.image ? (
+                <img src={msg.image} alt="" className='max-w-[230px] border 
+                border-gray-700 rounded-lg overflow-hidden mb-8' />
+              ):(
+                <p className={`p-2 max-w-[200px] md:text-sm font-light
+                  rounded-lg mb-8 break-all bg-violet-500/30 text-white 
+                  ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>
+                {msg.text}</p>
+              )}
+              <div className='text-center text-xs'>
+                <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} alt="" 
+                className='w-7 rounded-full' />
+                <p className='text-gray-500'>{formatMessageTime(msg.createdAt)}</p>
+              </div>
+            </div>
+           ))}
+           <div ref={scrollEnd}></div>
+         </div>
+
+         {/* _----bottom area */}
+         <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+            <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+                <input type="text" placeholder='Send a message' 
+                className='flex-1 text-sm p-3 border-none rounded-lg outline-none
+                text-white placeholder-gray-400'/>
+                <input type="file" id="image" accept='image/png, image/jpeg' hidden/>
+                <label htmlFor="image">
+                   <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cousor-pointer'/>
+                </label>
+            </div>
+            <img src={assets.send_button} alt="" className='w-7 cursor-pointer'/>
+         </div>
+
     </div>
   ) : (
       <div className='flex flex-col items-center justify-center gap-2 text-gray-500
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile page</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center
+    justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2
+      border-gray-600 flex items-center justify-between max-=sm:flex-col-reverse
+      rounded-lg'>
+        <form className='flex flex-col gap-5 p-10 flex-1'>
+          <h3>Profile details</h3>
+          <label htmlFor="avatar">
+              <input type="text" />
+              <img src="" alt="" />
+          </label>
+        </form>
+         <img src="" alt="" />
+      </div>
     </div>
   )
 }
